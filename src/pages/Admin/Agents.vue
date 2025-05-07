2dd0c6feb806e03a1cccb3f1f79194792ced0d6d
Asad Qureshi <asad.next.action@gmail.com>
2025-05-07 18:47:26 +0500

    Update

--- src/pages/Admin/Agents.vue
+++ src/pages/Admin/Agents.vue
@@ -124,8 +124,8 @@
   
 
   import Agentcard from "@/components/Agentcard.vue";
-  import Profile1 from "@/assets/images/Profile1.jpg";
-  import Profile2 from "@/assets/images/Profile2.jpg";
+  import Profile1 from "@/assets/images/profile1.jpg";
+  import Profile2 from "@/assets/images/profile2.jpg";
   
   // Component
   
